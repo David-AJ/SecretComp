1ea4a6ef1075db1c1994e6644ee21ab1b8656151
JiaoYuhang <812377857@qq.com>
2017-03-11 04:46:16 -0600

    update skl.py GBRT

--- skl.py
+++ skl.py
@@ -3,15 +3,18 @@ from sklearn import cross_validation as cv
 from sklearn.ensemble import GradientBoostingRegressor as GBR
 import Evaluation as e
 
-train_X = np.load("train_X.npy")
-train_Y = np.load("train_Y.npy")
+train_X = np.load("train_X2.npy")
+train_Y = np.load("train_Y2.npy")
 
-k_fold = cv.KFold(n=len(train_Y), n_folds=3)
+k_fold = cv.KFold(n=len(train_Y), n_folds=10)
 loss=[]
 for train,test in k_fold:
 	print "fitting GBRT..."
-	model =  GBR(n_estimators=100, learning_rate=0.01,max_depth=5, random_state=0,loss='huber').fit(train_X[train],train_Y[train])
+	model =  GBR(n_estimators=450, learning_rate=0.01,max_depth=10, random_state=0,loss='huber').fit(train_X[train],train_Y[train])
 	print "predicting..."
 	predict = model.predict(train_X[test])
-	loss.append(e.evalutaion(train_Y[test],predict))
+	core = e.evalutaion(train_Y[test],predict)
+	loss.append(core)
+	print core
+	break
 print np.array(loss).mean()
